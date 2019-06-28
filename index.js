814836ff33d07feef7d706202c79452c38f55c0f
Timur_Hook <davletshin.talan@gmail.com>
2019-06-28 08:52:25 -0700

    commit before going to work

---
+++ index.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var config = require('./config');
+var express = require('express');
+
+var socket = require('socket.io');
+
+var hookController = require('./controllers/hookController');
+var app = express();
+
+app.set('port',config.get('port'));
+app.set('view engine','ejs');
+app.use(express.static('./public'));
+
+
+var server = http.createServer(app).listen(config.get('port'),()=>{
+    console.log('Express server listening on port 2000')
+});
+
+var io = socket(server);
+hookController(app);
+
+io.on('connection', (socket)=>{
+    console.log('connection has been made');
+});
